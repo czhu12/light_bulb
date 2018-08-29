139f11af74b6fd2ff70558e04bc8a99f05a3478b
Chris Zhu <chris.zhu12@gmail.com>
2018-08-28 20:07:53 -0700

    remove pytorch as dependency

--- code/utils/model_builder.py
+++ code/utils/model_builder.py
@@ -1,8 +1,6 @@
-import torch
 import pdb
 import pickle
 import numpy as np
-from torch.autograd import Variable
 
 from dataset import Dataset
 from label import Label
@@ -19,58 +17,6 @@ from utils.text_utils import Tokenizer, UNKNOWN_TOKEN, EOS_TOKEN, PAD_TOKEN
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
-
-def to_numpy(x):
-    if type(x) in [np.ndarray, float, int]:
-        return x
-    elif isinstance(x, Variable):
-        return to_numpy(x.data)
-    else:
-        if x.is_cuda:
-            return x.cpu().numpy()
-        else:
-            return x.numpy()
-
-def load_lm_weights(
-        ds_id2word,
-        lm_id2word_path='vendor/keras_langauge_model.h5',
-        lm_model_path='vendor/keras_langauge_model.pkl',
-    ):
-    load_model()
-
-# Copy all encoder weights, and add our own weights
-def load_lm_weights(
-    ds_itos,
-    lm_weights_path='/Users/chris_zhu/Documents/Github/ulm-basenet/models/wt103/fwd_wt103.h5',
-    lm_itos_path='/Users/chris_zhu/Documents/Github/ulm-basenet/models/wt103/itos_wt103.pkl',
-):
-    lm_weights = torch.load(lm_weights_path, map_location=lambda storage, loc: storage)
-    lm_itos = pickle.load(open(lm_itos_path, 'rb'))
-    lm_stoi = {v:k for k,v in enumerate(lm_itos)}
-
-    ds_stoi = {v:k for k,v in enumerate(ds_itos)}
-    lm_words = set(lm_stoi.keys())
-    ds_words = set(ds_stoi.keys())
-    all_words = ds_words.union(lm_words)
-    n_tok = len(all_words)
-
-    # Adjust vocabulary to match finetuning corpus
-    lm_enc_weights = to_numpy(lm_weights['0.encoder.weight'])
-
-    tmp = np.zeros((n_tok, lm_enc_weights.shape[1]), dtype=np.float32)
-    tmp += lm_enc_weights.mean(axis=0)
-
-    all_itos = list(all_words)
-    for i, word in enumerate(all_words):
-        if word in lm_stoi:
-            tmp[i] = lm_enc_weights[lm_stoi[word]]
-
-    lm_weights['0.encoder.weight']                    = torch.Tensor(tmp.copy())
-    lm_weights['0.encoder_with_dropout.embed.weight'] = torch.Tensor(tmp.copy())
-    lm_weights['1.decoder.weight']                    = torch.Tensor(tmp.copy())
-
-    return lm_weights, all_itos
-
 
 class ModelBuilder:
     def __init__(self, dataset, label, config):
