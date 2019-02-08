ba363b07149da534e9c594bafffeebb2cde20bfa
Chris Zhu <chris.zhu12@gmail.com>
2019-02-07 19:50:50 -0800

    refactor server.py

--- light-bulb/server.py
+++ light-bulb/server.py
@@ -16,16 +16,223 @@ from dataset import MIN_UNSUPERVISED_EXAMPLES
 
 app = Flask(__name__, static_folder='ui/build/static', template_folder='ui/build')
 
+class Server():
+    """This wraps a label app and serves json information about it."""
+    def __init__(self, label_app):
+        self.label_app = label_app
+
+    def get_task(self):
+        valid_tokens = []
+        if hasattr(self.label_app.label_helper, 'valid_tokens'):
+            valid_tokens = self.label_app.label_helper.valid_tokens
+
+        classes = None
+        if hasattr(self.label_app.label_helper, 'classes'):
+            classes = self.label_app.label_helper.classes
+
+        default_class = None
+        if hasattr(self.label_app.label_helper, 'default_class'):
+            default_class = self.label_app.label_helper.default_class
+
+        return jsonify({
+            'title': self.label_app.title,
+            'description': self.label_app.description,
+            'template': self.label_app.template,
+            'label_type': self.label_app.label_helper.label_type,
+            'data_type': self.label_app.data_type,
+            'classes': classes,
+            'default_class': default_class,
+            'valid_tokens': valid_tokens,
+            "min_train": MIN_TRAIN_EXAMPLES,
+            "min_test": MIN_TEST_EXAMPLES,
+            "min_unsup": MIN_UNSUPERVISED_EXAMPLES,
+            "config": self.label_app.config,
+        })
+
+    def create_judgement(self):
+        try:
+            data = request.get_json()
+            id = data.get('id')
+            label = data.get('label')
+            time_taken = data.get('time_taken')
+            self.label_app.add_label(id, label, time_taken=time_taken)
+            return jsonify({'id': id, 'label': label})
+        except LabelError as e:
+            return jsonify({'error': e.message})
+
+    def create_judgements(self):
+        try:
+            data = request.get_json()
+            labels = data.get('labels')
+            avg_time_taken = data.get('time_taken')
+            self.label_app.add_labels(labels, avg_time_taken=avg_time_taken)
+            return jsonify(data)
+        except LabelError as e:
+            return jsonify({'error': e.message})
+
+    def labeled_data(self):
+        """
+        Returns labeled rows from the dataset.
+        """
+        page = int(request.args.get('page'))
+        page_size = int(request.args.get('page_size'))
+        labelled = True if request.args.get('labelled') == 'true' else False
+
+        batch, done = self.label_app.labelled_data(page, (page + 1) * page_size, labelled)
+        batch = batch.fillna('NaN')
+        return jsonify({
+            "dataset": list(batch.T.to_dict().values()),
+            "done": done,
+        })
+
+    def get_batch(self):
+        """
+        Returns a batch of items to be labelled and the phase
+        of the label collection (training or test)
+        """
+        if self.label_app.is_done:
+            return jsonify({
+                "done": True,
+                "batch": [],
+                "entropy": [],
+                "stage": [],
+                "y_prediction": [],
+            })
+
+        prediction = request.args.get('prediction') and request.args.get('prediction') == 'true'
+        reverse_entropy = request.args.get('reverse_entropy') and request.args.get('reverse_entropy') == 'true'
+
+        kwargs = {'prediction': prediction}
+        if request.args.get('force_stage'):
+            kwargs['force_stage'] = request.args.get('force_stage')
+
+        if request.args.get('sample_size'):
+            kwargs['size'] = int(request.args.get('sample_size'))
+        kwargs['reverse_entropy'] = reverse_entropy
+
+        batch, stage, y_prediction, entropies = self.label_app.next_batch(**kwargs)
+
+        batch = batch.fillna('NaN')
+        json_batch = jsonify({
+            "batch": list(batch.T.to_dict().values()),
+            "entropy": entropies,
+            "stage": stage,
+            "y_prediction": y_prediction,
+            "done": False,
+        })
+
+        return json_batch
+
+    def batch_items_batch(self):
+        # TODO: Fix this shit.
+        search_query = request.args.get('search_query')
+        target_size = 10 if self.label_app.label_helper.label_type == 'sequence' else 100
+        if search_query:
+            # if search query is present, we need to call label_app.search
+            batch = self.label_app.search(search_query)
+            target_class = -1
+        else:
+            batch, target_class = self.label_app.next_model_labelled_batch(target_size)
+
+        if len(batch) == 0:
+            return jsonify({
+                "batch": [],
+                "target_class": 0,
+                "done": True,
+            })
+        else:
+            batch = batch.fillna('NaN')
+            json_batch = jsonify({
+                "batch": list(batch.T.to_dict().values()),
+                "target_class": target_class,
+                "done": False,
+            })
+
+            return json_batch
+
+    def get_history(self):
+        """
+        Get training history for label app.
+        """
+        history = {
+            "history": self.label_app.get_history().history,
+        }
+        stats = self.label_app.get_stats()
+        return jsonify({
+            **history,
+            **stats,
+        })
+
+    def score(self):
+        """
+        Image Classification:
+        {
+            "type": "images",
+            "urls": ["https://my-url.com/image.jpg"],
+        }
+        Text Classification:
+        {
+            "type": "texts",
+            "texts": ["the text that i want to classify"],
+        }
+        Sequence Classification:
+        {
+            "type": "sequence",
+            "texts": ["the sequence that i want to label"],
+        }
+        """
+        json = request.get_json()
+
+        _type = json['type']
+        if _type == "images":
+            urls = json["urls"]
+            x_train, images = utils.download_urls(urls)
+            scores = self.label_app.score(x_train)
+        elif _type == "text" or _type == "sequence":
+            texts = json["texts"]
+            scores = self.label_app.score(texts)
+        return jsonify({'scores': scores.tolist(), 'labels': self.label_app.label_helper.classes})
+
+    def predict(self):
+        """
+        Image Classification:
+        {
+            "type": "images",
+            "urls": ["https://my-url.com/image.jpg"],
+        }
+        Text Classification:
+        {
+            "type": "texts",
+            "texts": ["the text that i want to classify"],
+        }
+        Sequence Classification:
+        {
+            "type": "sequence",
+            "texts": ["the sequence that i want to label"],
+        }
+        """
+        json = request.get_json()
+
+        _type = json['type']
+        if _type == "images":
+            urls = json["urls"]
+            x_train, images = utils.download_urls(urls)
+            predictions = self.label_app.predict(x_train)
+        elif _type == "text" or _type == "sequence":
+            texts = json["texts"]
+            predictions = self.label_app.predict(texts)
+        return jsonify({'predictions': predictions.tolist(), **json})
+
 @app.route('/')
 @app.route('/dataset')
 def index():
     return render_template(
         'index.html',
-        data_type=label_app.data_type,
-        title=label_app.title,
-        description=label_app.description,
-        label_helper=label_app.label_helper,
-        label_type=label_app.label_helper.label_type,
+        data_type=server.label_app.data_type,
+        title=server.label_app.title,
+        description=server.label_app.description,
+        label_helper=server.label_app.label_helper,
+        label_type=server.label_app.label_helper.label_type,
     )
 
 @app.route('/task', methods=['GET'])
@@ -37,110 +244,23 @@ def task():
             description,
         }
     """
-    valid_tokens = []
-    if hasattr(label_app.label_helper, 'valid_tokens'):
-        valid_tokens = label_app.label_helper.valid_tokens
-
-    classes = None
-    if hasattr(label_app.label_helper, 'classes'):
-        classes = label_app.label_helper.classes
-
-    default_class = None
-    if hasattr(label_app.label_helper, 'default_class'):
-        default_class = label_app.label_helper.default_class
-
-    return jsonify({
-        'title': label_app.title,
-        'description': label_app.description,
-        'template': label_app.template,
-        'label_type': label_app.label_helper.label_type,
-        'data_type': label_app.data_type,
-        'classes': classes,
-        'default_class': default_class,
-        'valid_tokens': valid_tokens,
-        "min_train": MIN_TRAIN_EXAMPLES,
-        "min_test": MIN_TEST_EXAMPLES,
-        "min_unsup": MIN_UNSUPERVISED_EXAMPLES,
-        "config": label_app.config,
-    })
+    return server.get_task()
 
 @app.route('/judgements', methods=['POST'])
 def create_judgement():
-    try:
-        json = request.get_json()
-        id = json.get('id')
-        label = json.get('label')
-        time_taken = json.get('time_taken')
-        label_app.add_label(id, label, time_taken=time_taken)
-        return jsonify({'id': id, 'label': label})
-    except LabelError as e:
-        return jsonify({'error': e.message})
+    return server.create_judgement()
 
 @app.route('/judgements/batch', methods=['POST'])
 def create_judgements():
-    try:
-        json = request.get_json()
-        labels = json.get('labels')
-        avg_time_taken = json.get('time_taken')
-        label_app.add_labels(labels, avg_time_taken=avg_time_taken)
-        return jsonify(json)
-    except LabelError as e:
-        return jsonify({'error': e.message})
+    return server.create_judgements()
 
 @app.route('/api/dataset', methods=['GET'])
 def labeled_data():
-    """
-    Returns labeled rows from the dataset.
-    """
-    page = int(request.args.get('page'))
-    page_size = int(request.args.get('page_size'))
-    labelled = True if request.args.get('labelled') == 'true' else False
-
-    batch, done = label_app.labelled_data(page, (page + 1) * page_size, labelled)
-    batch = batch.fillna('NaN')
-    return jsonify({
-        "dataset": list(batch.T.to_dict().values()),
-        "done": done,
-    })
+    return server.labeled_data()
 
 @app.route('/batch', methods=['GET'])
 def batch():
-    """
-    Returns a batch of items to be labelled and the phase
-    of the label collection (training or test)
-    """
-    if label_app.is_done:
-        return jsonify({
-            "done": True,
-            "batch": [],
-            "entropy": [],
-            "stage": [],
-            "y_prediction": [],
-        })
-
-    prediction = request.args.get('prediction') and request.args.get('prediction') == 'true'
-    reverse_entropy = request.args.get('reverse_entropy') and request.args.get('reverse_entropy') == 'true'
-
-    kwargs = {'prediction': prediction}
-    if request.args.get('force_stage'):
-        kwargs['force_stage'] = request.args.get('force_stage')
-
-    if request.args.get('sample_size'):
-        kwargs['size'] = int(request.args.get('sample_size'))
-    kwargs['reverse_entropy'] = reverse_entropy
-
-    batch, stage, y_prediction, entropies = label_app.next_batch(**kwargs)
-
-    batch = batch.fillna('NaN')
-    json_batch = jsonify({
-        "batch": list(batch.T.to_dict().values()),
-        "entropy": entropies,
-        "stage": stage,
-        "y_prediction": y_prediction,
-        "done": False,
-    })
-
-    return json_batch
+    return server.get_batch()
 
 
 def _df_to_jsonable(df):
@@ -159,44 +279,11 @@ def _df_to_jsonable(df):
 @app.route('/batch_items_batch', methods=['GET'])
 def batch_items_batch():
     # TODO: Fix this shit.
-    search_query = request.args.get('search_query')
-    target_size = 10 if label_app.label_helper.label_type == 'sequence' else 100
-    if search_query:
-        # if search query is present, we need to call label_app.search
-        batch = label_app.search(search_query)
-        target_class = -1
-    else:
-        batch, target_class = label_app.next_model_labelled_batch(target_size)
-
-    if len(batch) == 0:
-        return jsonify({
-            "batch": [],
-            "target_class": 0,
-            "done": True,
-        })
-    else:
-        batch = batch.fillna('NaN')
-        json_batch = jsonify({
-            "batch": list(batch.T.to_dict().values()),
-            "target_class": target_class,
-            "done": False,
-        })
-
-        return json_batch
+    return server.batch_items_batch()
 
 @app.route('/history', methods=['GET'])
 def history():
-    """
-    Get training history for label app.
-    """
-    history = {
-        "history": label_app.get_history().history,
-    }
-    stats = label_app.get_stats()
-    return jsonify({
-        **history,
-        **stats,
-    })
+    return server.get_history()
 
 @app.route('/evalution', methods=['GET'])
 def evaluation():
@@ -216,74 +303,20 @@ def get_image():
 def demo():
     return render_template(
         'index.html',
-        data_type=label_app.data_type,
-        title=label_app.title,
-        description=label_app.description,
-        label_helper=label_app.label_helper,
-        label_type=label_app.label_helper.label_type,
+        data_type=server.label_app.data_type,
+        title=server.label_app.title,
+        description=server.label_app.description,
+        label_helper=server.label_app.label_helper,
+        label_type=server.label_app.label_helper.label_type,
     )
 
 @app.route("/score", methods=['POST', 'PUT', 'GET'])
 def score():
-    """
-    Image Classification:
-    {
-        "type": "images",
-        "urls": ["https://my-url.com/image.jpg"],
-    }
-    Text Classification:
-    {
-        "type": "texts",
-        "texts": ["the text that i want to classify"],
-    }
-    Sequence Classification:
-    {
-        "type": "sequence",
-        "texts": ["the sequence that i want to label"],
-    }
-    """
-    json = request.get_json()
-
-    _type = json['type']
-    if _type == "images":
-        urls = json["urls"]
-        x_train, images = utils.download_urls(urls)
-        scores = label_app.score(x_train)
-    elif _type == "text" or _type == "sequence":
-        texts = json["texts"]
-        scores = label_app.score(texts)
-    return jsonify({'scores': scores.tolist(), 'labels': label_app.label_helper.classes})
+    return server.score()
 
 @app.route("/predict", methods=['POST', 'PUT', 'GET'])
 def predict():
-    """
-    Image Classification:
-    {
-        "type": "images",
-        "urls": ["https://my-url.com/image.jpg"],
-    }
-    Text Classification:
-    {
-        "type": "texts",
-        "texts": ["the text that i want to classify"],
-    }
-    Sequence Classification:
-    {
-        "type": "sequence",
-        "texts": ["the sequence that i want to label"],
-    }
-    """
-    json = request.get_json()
-
-    _type = json['type']
-    if _type == "images":
-        urls = json["urls"]
-        x_train, images = utils.download_urls(urls)
-        predictions = label_app.predict(x_train)
-    elif _type == "text" or _type == "sequence":
-        texts = json["texts"]
-        predictions = label_app.predict(texts)
-    return jsonify({'predictions': predictions.tolist(), **json})
+    return server.predict()
 
 @app.route('/css/index.css')
 def root():
@@ -297,8 +330,9 @@ def root():
     no_train=("Don't train model.", "flag", "d", bool),
 )
 def main(config, port=5000, mode="training", log_level='DEBUG', no_train=False):
-    global label_app
+    global server
     label_app = LabelApp.load_from({'path': config, 'log_level': log_level})
+    server = Server(label_app)
 
     if not no_train:
         train_thread = Thread(target=label_app.threaded_train)
